e3650735932cd24043fbbed4ce99a6b4ad89a5b7
Wilson Neto B.R <contato@wilsonneto.com.br>
2021-11-15 01:26:23 -0300

    Update Startup.cs

--- ApiFake/Startup.cs
+++ ApiFake/Startup.cs
@@ -43,7 +43,7 @@ namespace ApiFake
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiFake v1");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "ApiFake v2");
                 c.RoutePrefix = string.Empty;
             });
 
